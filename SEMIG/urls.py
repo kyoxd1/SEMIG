26f3b58126e4490b307288d9f641a33cd640da7e
AlejoLuis <kyomaster1234@gmail.com>
2023-09-19 08:21:40 -0400

    configs

--- SEMIG/urls.py
+++ SEMIG/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from SEMIG.settings import base
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('questionsRules.urls'))
-]
+] + static(base.STATIC_URL, document_root=base.STATIC_ROOT)
